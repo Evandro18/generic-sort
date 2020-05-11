32e487f0c258c8a69336be251bdc3b314d94d61d
Evandro18 <evandro18.goncalves@gmail.com>
2020-05-11 16:10:13 -0400

    generic sort function

---
+++ index.js
@@ -0,0 +1,29 @@
+const _get = require('lodash/get')
+
+exports.genericSort = ({ list = [], label = '', func, asc = true }) => {
+	const compare = (a, b) => {
+		if (typeof a === 'string' && typeof b === 'string') {
+			return asc ? a.localeCompare(b) : b.localeCompare(a)
+		}
+		if (asc) return a > b ? 1 : -1
+		if (!asc) return a > b ? -1 : 1
+	}
+
+	return list.sort((a, b) => {
+		if (func && label) {
+			const formatA = func(_get(a, label))
+			const formatB = func(_get(b, label))
+			return compare(formatA, formatB)
+		}
+		if (!func && label) {
+			const formatA = _get(a, label)
+			const formatB = _get(b, label)
+			return compare(formatA, formatB)
+		}
+
+		if (label) {
+			return compare(_get(a, label), _get(b, label))
+		}
+		return compare(a, b)
+	})
+}
---
+++ teste.js
@@ -0,0 +1,2 @@
+const { genericSort } = require('./index')
+console.log(genericSort({ list: [45, 23, 76, 123, 1, 3] }))
